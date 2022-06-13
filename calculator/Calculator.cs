09ee9c212e0f6f5f4da90724332a82f6d3557894
sobhangit <sobhanpe2012@gmail.com>
2022-06-13 14:44:52 +0430

    old project import

---
+++ calculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Calculator
+    {
+        public static int number1 = 0;
+        string operate = "";
+
+        public static void calculate(int num1, int num2, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    number1 = sum(num1, num2);
+                    break;
+                case "-":
+                    number1 = minus(num1, num2);
+                    break;
+                case "*":
+                    number1 = multiply(num1, num2);
+                    break;
+                case "/":
+                    number1 = divide(num1, num2);
+                    break;
+                case "=":
+                    showResult();
+                    break;
+            }
+
+        }
+
+        static int sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static int minus(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        static int multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        static int divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+
+        public static void showResult()
+        {
+            Console.WriteLine("result : {0}", number1);
+        }
+
+        public static void cleanCalculator()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            showResult();
+            number1 = 0;
+            Console.ResetColor();
+        }
+
+    }
+}
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,12 +1,94 @@
 ﻿using System;
+using calculator;
 
-namespace calculator
+Console.WriteLine("Calculator [ + , - , * , / ]");
+Console.WriteLine();
+
+int number2;
+string operate;
+bool isCleanCalculator = true;
+
+while (true)
 {
-    class Program
+    if (isCleanCalculator)
     {
-        static void Main(string[] args)
+        Calculator.number1 = getNumber();
+        isCleanCalculator = false;
+    }
+
+    operate = getOperator();
+
+    if (operate == "=")
+    {
+        Calculator.cleanCalculator();
+        break;
+    }
+
+    number2 = getNumber();
+
+    Calculator.calculate(Calculator.number1, number2, operate);
+    Calculator.showResult();
+}
+
+isCleanCalculator = true;
+
+static int getNumber()
+{
+    string userInput;
+    int number = 0;
+
+    Console.Write("enter a integer number : ");
+    userInput = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(userInput))
+    {
+        number = getNumber();
+    }
+    else
+    {
+        if (int.TryParse(userInput, out number))
         {
-            Console.WriteLine("Hello World!");
+            number = int.Parse(userInput);
+        }
+        else
+        {
+            number = getNumber();
         }
     }
+
+    return number;
+}
+
+static string getOperator()
+{
+    string operate;
+
+    Console.Write("enter a operator : ");
+    operate = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(operate))
+    {
+        operate = getOperator();
+    }
+    else
+    {
+        switch (operate)
+        {
+            case "+":
+                return operate;
+            case "-":
+                return operate;
+            case "*":
+                return operate;
+            case "/":
+                return operate;
+            case "=":
+                return operate;
+            default:
+                operate = getOperator();
+                break;
+        }
+    }
+
+    return operate;
 }
